fca23a4b9f441acbfb3678b3b18fa354af7041ef
Wafaa Wardah <wafaawardah89@gmail.com>
2022-04-30 05:16:19 +0200

    Added math-game-1-simple.py file.

---
+++ math-game-1-simple.py
@@ -0,0 +1,24 @@
+# This is a simple Arithmetic quiz
+# Python concepts: variables, data types, user input
+
+print("\n----------- ARITHMETIC GAME -----------\n")
+name = input("Enter name: ")
+
+# Question 1
+a = 5
+b = 7
+answer = input("\n" + str(a) + " + " + str(b) + " = ")
+print(a, "+", b, "=", a+b)
+
+# Question 2
+a = 8
+b = 3
+answer = input("\n" + str(a) + " x " + str(b) + " = ")
+print(a, "+", b, "=", a*b)
+
+# Question 3
+a = 54
+b = 6
+answer = input("\n" + str(a) + " - " + str(b) + " = ")
+print(a, "+", b, "=", a-b)
+
